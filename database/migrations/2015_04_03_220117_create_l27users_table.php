a076aaa31318d73a176a32e29748d29249d1137a
David Cohen <dcohen613@gmail.com>
2015-04-03 22:03:08 +0000

    Remove scaffolding and add l27users table

--- database/migrations/2015_04_03_220117_create_l27users_table.php
+++ database/migrations/2015_04_03_220117_create_l27users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateL27usersTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,13 +12,12 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		Schema::create('l27users', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
-			$table->string('email')->unique();
-			$table->string('password', 60);
-			$table->rememberToken();
+      $table->string('api_key');
+      $table->string('subdomain');
+      $table->string('url');
 			$table->timestamps();
 		});
 	}
@@ -30,7 +29,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop('l27users');
 	}
 
 }
